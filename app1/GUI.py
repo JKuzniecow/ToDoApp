8da799952bd1739fc53aa98279a90db1f931ffd4
Julia <julia.kuzniecow@gmail.com>
2023-03-06 19:40:26 +0100

    finishing GUI

--- app1/GUI.py
+++ app1/GUI.py
@@ -2,6 +2,12 @@ import functions
 import PySimpleGUI as sg
 import time
 
+# To create an exe.file for Win + use pyinstaller
+# import os
+# if not os.path.exists("todos.txt"):
+    # with open("todos.txt", "w") as file:
+        # pass
+
 sg.theme("DarkBlue3")
 
 clock = sg.Text("", key="clock")
@@ -9,7 +15,7 @@ label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip="Enter new task", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
-                      enable_events=True, size=[45, 10])
+                      enable_events=True, size=[44, 10])
 edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
